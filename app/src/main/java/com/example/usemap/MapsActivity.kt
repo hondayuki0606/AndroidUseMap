637c75b247cd4d812325fb95606a74120c3c82db
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-06-12 22:03:27 +0900

    Permission Check implemantation

--- app/src/main/java/com/example/usemap/MapsActivity.kt
+++ app/src/main/java/com/example/usemap/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.usemap
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,31 +17,67 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        // 赤い波線でエラーが表示されてしまうので
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED){
+            myLocationEnable()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            //許可を求め、拒否されていた場合
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+         } else {
+             // まだ許可を求めていない
+             ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                     MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION)
+         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION->{
+                if(permissions.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    // 許可された
+                    myLocationEnable()
+                } else {
+                    showToast("現在位置は表示できません")
+                }
+            }
+        }
+    }
+
+    private fun myLocationEnable() {
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            mMap.isMyLocationEnabled = true
+        }
+    }
+
+    private fun showToast(msg: String) {
+        val toast = Toast.makeText(this,msg,Toast.LENGTH_LONG)
+        toast.show()
     }
 }
